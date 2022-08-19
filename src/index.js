66b4452e8ddff0c52f6bbfb0092716602109c693
Michael Robards <mikerobards@icloud.com>
2022-08-19 09:07:46 -0400

    add App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import CardList from "./CardList";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 import { robots } from "./robots";
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
-      <CardList robots={robots} />
+      <App />
     </>
   </React.StrictMode>
 );
